9cf42a0e7e28802e8fe1676b4dc1f3d71b31a9c3
Irine Egorenkova <17315833+IrineEgo@users.noreply.github.com>
2018-11-02 19:48:32 +0300

    Create create-picture.php

--- create-picture.php
+++ create-picture.php
@@ -1,24 +1,22 @@
 <?php
     header('Content-type: image/png');
+    $testname = $_POST['testname'];
     $username = $_POST['username'];
     $date = $_POST['date'];
-    //$correct_answers = $_POST['correct_answers'];
-    //$answers = $_POST['answers'];
-    //$correct = $correct_answers . ' из ' . $answers;
-        // Создаем картинку и цвет шрифта
-        $img = imagecreatefromjpeg('img/picture.jpg');
-        $fontColor = imagecolorallocate($img, 035, 035, 035);
-        // Определяем положение имени пользователя (по центру)
-        $imageWidth = getimagesize('img/picture.jpg');
-        $imageWidth = $imageWidth[0];
-        $textPoints = imagettfbbox(300, 0, 'fonts/OpenSans.ttf', $username);
-        $textWidth = $textPoints[2] - $textPoints[0];
-        $x = ($imageWidth - $textWidth) / 2;
-        // Распологаем все данные на картинке
-        imagettftext($img, 300, 0, $x, 1000, $fontColor, 'fonts/OpenSans.ttf', $username);
-        imagettftext($img, 200, 0, 900, 1625, $fontColor, 'fonts/OpenSans.ttf', $test['name']);
-        imagettftext($img, 180, 0, 1600, 1976, $fontColor, 'fonts/OpenSans.ttf', $correct_answers);
-        imagettftext($img, 150, 0, 1600, 2325, $fontColor, 'fonts/OpenSans.ttf', $date);
-        imagepng($img);
-        imagedestroy($img);
+    $correctAnswers = $_POST['correctAnswers'];
+    $totalAnswers = $_POST['totalAnswers'];
+    $answers = $correctAnswers . ' из ' . $totalAnswers;
+    $img = imagecreatefromjpeg('img/picture.jpg');
+    $white = imagecolorallocate($img, 035, 035, 035);
+    $imageWidth = getimagesize('img/picture.jpg');
+    $imageWidth = $imageWidth[0];
+    $textPoints = imagettfbbox(300, 0, 'fonts/OpenSans.ttf', $username);
+    $textWidth = $textPoints[2] - $textPoints[0];
+    $x = ($imageWidth - $textWidth) / 2;
+    imagettftext($img, 300, 0, $x, 1000, $white, 'fonts/OpenSans.ttf', $username . '!');
+    imagettftext($img, 200, 0, 1600, 1625, $white, 'fonts/OpenSans.ttf', $testname);
+    imagettftext($img, 180, 0, $x, 1976, $white, 'fonts/OpenSans.ttf', $answers);
+    imagettftext($img, 150, 0, 1600, 2325, $white, 'fonts/OpenSans.ttf', $date);
+    imagepng($img);
+    imagedestroy($img);
 ?>
