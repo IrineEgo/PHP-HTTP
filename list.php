e276977b830802370f5b068209d35d8671fc57dd
Irine Egorenkova <17315833+IrineEgo@users.noreply.github.com>
2018-11-02 19:51:32 +0300

    Update list.php

--- list.php
+++ list.php
@@ -7,28 +7,38 @@
   <head>
     <meta charset="utf-8">
     <title>Список тестов</title>
+	  <style type="text/css">
+	    h1 {
+	      text-align: center;
+	    }	  
+        a {
+          color: navy;
+          text-decoration: none;
+        }
+		.file-block {
+	      margin: 40px auto 20px;
+		  width: 40%;
+		  border-bottom: 1px solid navy;
+        }
+      </style> 	
   </head>
   <body>
-    <a href="admin.php"><div>&laquo; НАЗАД</div></a>
-    <hr>
-    <h2>Список тестов:</h2>
+    <a href="admin.php"><b>&laquo; НАЗАД</b></a><br><br>
+
+    <h1>Список тестов:</h1>
     <?php if (!empty($allFiles)): 
-    ?>
+	?>
         <?php foreach ($allFiles as $file): 
-	?>
-            <?php
-                $file_data = json_decode(file_get_contents($file));
-            ?>
-            <div>
-                <h3><?=$file_data->name?></h3>
-                <a href="test.php?number=<?php echo array_search($file, $allFiles); ?>">ПРОЙТИ ТЕСТ &raquo;</a>
+		?>
+            <div class="file-block">
+                <h3><?php echo str_replace('tests/', '', $file); ?></h3>
+                <a href="test.php?number=<?php echo array_search($file, $allFiles); ?>">ПРОЙТИ ТЕСТ &raquo;</a><br><br>
             </div>
-            <hr>
         <?php endforeach; 
-	?>
+		?>
         <?php endif; 
-	?>
+		?>
     <?php if (empty($allFiles)) echo 'Нет ни одного теста!';
-    ?>	
+	?>	
   </body>
 </html>
